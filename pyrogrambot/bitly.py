d1e058c1428a347f83df3e2beaf78df508e16b27
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-07-23 16:01:01 +0530

    Update bitly.py

--- pyrogrambot/bitly.py
+++ pyrogrambot/bitly.py
@@ -3,10 +3,6 @@ import requests
 import json 
 import os
  
-@Client.on_message(filters.private & filters.command(['bitly']))
-async def start(client,message):
-  await message.reply_text(f"Hello {message .from_user.first_name}\ni am bit.ly short link genrator\n made with love by @tedzosir \n send yourl link \n i converte your link", reply_to_message_id = message.message_id)
-  
 @Client.on_message(filters.private & filters.regex("http|https"))
 async def Bitly(client,message):
   URL = message.text
