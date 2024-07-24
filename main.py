bcd117c018ef0e5557fb8997d653a7f318ecb8ce
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-24 19:43:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from pyrogram import Client
 import os
+API_ID = '15453419'
+API_HASH = '6c9c9e5a2e65daf192e7dd9dde026f45'
+BOT_TOKEN = '7384862816:AAHUwGihYbI63mDdMSFR78H6qB8LIpMzLXw'
+# Define a list of prank messages
 
-Pyrogrambot = Client(
+# Initialize the Clien
+
+load_dotenv()
+
+pyrogrambot= Client(
     "Pyrogram Bot",
-    bot_token = os.environ["7384862816:AAHUwGihYbI63mDdMSFR78H6qB8LIpMzLXw"],
-    api_id = int(os.environ["15453419"]),
-    api_hash = os.environ["6c9c9e5a2e65daf192e7dd9dde026f45"],
-    plugins=dict(root="pyrogrambot")
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    workers=100,
+    plugins=dict(root="plugins")
 )
 
 Pyrogrambot.run()
