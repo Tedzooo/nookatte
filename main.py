8e738a09edad9f759188ca21f502e834e50f90e7
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-26 14:05:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 import os
 API_ID = '15453419'
 API_HASH = '6c9c9e5a2e65daf192e7dd9dde026f45'
-BOT_TOKEN = '7384862816:AAHUwGihYbI63mDdMSFR78H6qB8LIpMzLXw'
+BOT_TOKEN = '7384862816:AAHvxhtB-4rroiSlZmbgf0MUfKP25hNpmeA'
 # Define a list of prank messages
 
 # Initialize the Clien
